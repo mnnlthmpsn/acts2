8eb419869a3023db8b91f1863ff407b5bd74771a
unknown <mnnlthmpsn@outlook.com>
2020-04-09 01:01:48 -0700

    gzip for whitenoise

--- acts2/settings.py
+++ acts2/settings.py
@@ -127,6 +127,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
